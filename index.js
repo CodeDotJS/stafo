97a48dae73b655b641a5b563de549e9942b3178a
CodeDotJS <rushforlinux@gmail.com>
2016-06-21 21:10:43 +0530

    :tada: clean

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const got = require('got');
+const cheerio = require('cheerio');
+const Promise = require('pinkie-promise');
+
+function stafo(repository) {
+	if (typeof repository !== 'string') {
+		return Promise.reject(new Error('repository required'));
+	}
+
+	var url = 'https://www.github.com/' + repository;
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return {
+			star: $('.social-count').eq(1).text().trim() || null,
+			fork: $('.social-count').eq(2).text().trim() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'repository doesn\'t exits';
+		}
+		throw err;
+	});
+}
+
+function gitStar(repository) {
+	if (typeof repository !== 'string') {
+		return Promise.reject(new Error('repository required'));
+	}
+	var url = 'https://www.github.com/' + repository;
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return $('.social-count').eq(1).text().trim() || null;
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'repository doesn\'t exist';
+		}
+		throw err;
+	});
+}
+
+function gitFork(repository) {
+	if (typeof repository !== 'string') {
+		return Promise.reject(new Error('repository required'));
+	}
+	var url = 'https://www.github.com/' + repository;
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return $('.social-count').eq(2).text().trim() || null;
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'repository doesn\'t exist';
+		}
+		throw err;
+	});
+}
+
+exports.repo = stafo;
+exports.stars = gitStar;
+exports.forks = gitFork;
