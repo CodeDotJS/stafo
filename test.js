396e1801389ca200472d3b495da863ff2f1a1d1b
CodeDotJS <rushforlinux@gmail.com>
2016-06-21 21:36:55 +0530

    change gitStar value

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ test(async t => {
 	const gitStar = await fn.stars('facebook/react');
 	const gitFork = await fn.forks('facebook/react');
 
-	t.is(gitStar, '44,022');
+	t.is(gitStar, '44,024');
 	t.is(gitFork, '7,587');
 });
